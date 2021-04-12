9951cadb4d23c2769f1fc7c2137414a67161adf5
Simonas Bazinys <sbazinys@gmail.com>
2021-04-12 19:53:21 +0300

    pijaus testai

--- src/test/java/com/accouting/CodeValueTest.java
+++ src/test/java/com/accouting/CodeValueTest.java
@@ -1,13 +1,10 @@
-package com.accouting;
-
-
 import com.accouting.utilities.DataValidity;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class CodeValue_tests {
+public class CodeValueTest {
         @Test
         public void numberWithLessThan6symbolShouldBeIncorrect()
         {
